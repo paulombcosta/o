e1567248d5aecf2fe58d1fb1ebc091cb5cb87dbd
Paulo Costa <paulombcosta@gmail.com>
2022-07-11 08:16:11 +0200

    Fixing command not working if parent has value

--- o/src/main.rs
+++ o/src/main.rs
@@ -13,7 +13,10 @@ fn main() -> Result<(), Error> {
 
     let matches = command.get_matches();
 
-    let matched_cmd = get_command_from_config_file(matches, config.cmds);
+    let last_command = env::args().last().unwrap();
+
+    let matched_cmd = get_command_from_config_file(matches, config.cmds, last_command);
+
 
     match matched_cmd {
         Some(v) => webbrowser::open(&v)?,
@@ -23,30 +26,30 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn get_command_from_config_file(cli_args: ArgMatches, config_file_cmds: Vec<Cmd>) -> Option<String> {
+fn get_command_from_config_file(cli_args: ArgMatches, config_file_cmds: Vec<Cmd>, last_cmd: String) -> Option<String> {
         config_file_cmds
         .iter()
-        .map(|c| get_match_command(&cli_args, c))
+        .map(|c| get_match_command(&cli_args, c, &last_cmd))
         .filter(|result| result.is_some())
         .next()
         .unwrap()
 }
 
-fn get_match_command(argmatch: &ArgMatches, cmd: &Cmd) -> Option<String> {
+fn get_match_command(argmatch: &ArgMatches, cmd: &Cmd, last_cmd: &String) -> Option<String> {
     match argmatch.subcommand() {
         Some((name, sub_matches)) => {
             if name != cmd.name {
                 return None;
             }
 
-            if cmd.value.is_some() {
+             if cmd.value.is_some() && cmd.name == last_cmd.to_string() {
                 return cmd.value.clone();
             };
 
             match &cmd.children {
                 Some(ch) => ch
                     .iter()
-                    .map(|c| get_match_command(sub_matches, c))
+                    .map(|c| get_match_command(sub_matches, c, last_cmd))
                     .filter(|m| m.is_some())
                     .next()
                     .unwrap(),
@@ -63,32 +66,40 @@ mod tests {
     use super::*;
     use o_core::config::{get_config};
 
-    fn get_matches(cli_args: Vec<&str>) -> (ArgMatches, Vec<Cmd>) {
+    fn get_matches(cli_args: Vec<&str>) -> (ArgMatches, Vec<Cmd>, String) {
+        let last_arg = cli_args.last().unwrap().to_string();
         let config = get_config(PathBuf::from("../test"));
         let commands: Vec<Command> = config.cmds.iter().map(|c| create_command(c)).collect();
         let command = command!().subcommands(commands);
-        (command.get_matches_from(cli_args), config.cmds)
+        (command.get_matches_from(cli_args), config.cmds, last_arg)
     }
 
     #[test]
     fn test_should_extract_single_value() {
-        let (matches, cmds)= get_matches(vec!["o", "reddit"]);
-        let matched_cmd = get_command_from_config_file(matches, cmds);
+        let (matches, cmds, last_arg)= get_matches(vec!["o", "reddit"]);
+        let matched_cmd = get_command_from_config_file(matches, cmds, last_arg);
         assert_eq!(matched_cmd, Some(String::from("https://reddit.com")));
     }
 
     #[test]
     fn test_should_get_value_from_nested_command() {
-        let (matches, cmds)= get_matches(vec!["o", "github", "druid"]);
-        let matched_cmd = get_command_from_config_file(matches, cmds);
+        let (matches, cmds, last_arg)= get_matches(vec!["o", "github", "druid"]);
+        let matched_cmd = get_command_from_config_file(matches, cmds, last_arg);
         assert_eq!(matched_cmd, Some(String::from("https://github.com/linebender/druid")));
     }
 
     #[test]
     fn test_should_open_base_value_even_if_children_is_present() {
-        let (matches, cmds)= get_matches(vec!["o", "sample"]);
-        let matched_cmd = get_command_from_config_file(matches, cmds);
+        let (matches, cmds, last_arg)= get_matches(vec!["o", "sample"]);
+        let matched_cmd = get_command_from_config_file(matches, cmds, last_arg);
         assert_eq!(matched_cmd, Some(String::from("http://base.sample.com")));
+    }
+    
+    #[test]
+    fn test_should_use_children_value_even_if_parent_has_value() {
+        let (matches, cmds, last_arg)= get_matches(vec!["o", "sample", "nested1"]);
+        let matched_cmd = get_command_from_config_file(matches, cmds, last_arg);
+        assert_eq!(matched_cmd, Some(String::from("http://nested1.com")));
     }
 
 }
